95d627f97bf9921a3767d6a6c5ff5dca0571e8b1
Andreas Gasser <andreas.safe@gmail.com>
2018-06-27 07:45:05 +0200

    finished work on get image with user and image id

--- gateway/resources/user/image/get_with_id/src/index.js
+++ gateway/resources/user/image/get_with_id/src/index.js
@@ -1,20 +1,42 @@
 'use strict';
 
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = function (event, context, callback) {
     console.log(event);
-    console.log(context);
-
-    const message = {
-        event,
-        context,
-    };
-
-    var response = {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'text/html; charset=utf-8',
+    console.log(event.pathParameters);
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: {
+            imageId: event.pathParameters.imageId,
+            userId: event.pathParameters.userId,
         },
-        body: "<p>" + JSON.stringify(message) + "</p>",
     };
-    callback(null, response);
+
+    // fetch sample from the database
+    dynamoDb.get(params, (error, result) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            callback(null, {
+                statusCode: error.statusCode || 501,
+                headers: {
+                    'Content-Type': 'text/plain',
+                    'Access-Control-Allow-Origin': event.headers.origin
+                },
+                body: 'Couldn\'t fetch the sample item.',
+            });
+            return;
+        }
+
+        // create a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result.Item),
+            headers: { 'Access-Control-Allow-Origin': event.headers.origin },
+        };
+        callback(null, response);
+    });
 };
